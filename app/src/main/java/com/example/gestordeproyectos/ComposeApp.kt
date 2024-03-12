12ab4141dea49ca72ec1396210afedc85d3312fa
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2024-03-12 15:38:04 -0400

    update

---
+++ app/src/main/java/com/example/gestordeproyectos/ComposeApp.kt
@@ -0,0 +1,6 @@
+package com.example.gestordeproyectos
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+@HiltAndroidApp
+class ComposeApp: Application()
--- app/src/main/java/com/example/gestordeproyectos/MainActivity.kt
+++ app/src/main/java/com/example/gestordeproyectos/MainActivity.kt
@@ -12,13 +12,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gestordeproyectos.ui.theme.GestorDeProyectosTheme
 import com.example.gestordeproyectos.ui.theme.login.LoginScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GestorDeProyectosTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
